55fdbb75734f320e5a1da137c2cad9cb27a969ea
henok <hassef1@students.towson.edu>
2021-04-11 17:54:31 -0400

    signUp mod

--- share/website/auth.py
+++ share/website/auth.py
@@ -2,7 +2,7 @@ from flask import Blueprint, render_template, flash
 
 auth = Blueprint('auth', __name__)
 
-@auth.route('/lo')
+
 
 @auth.route ('/login', methods = ['GET', 'POST'])
 def login():
@@ -15,21 +15,21 @@ def logout():
 
 @auth.route ('/sign-up', methods = ['GET', 'POST'])
 def sign_up():
-    if request.method == 'POST':
-        email = request.form.get('email')
-        firstName = request.form.get('firstName')
-        password1 = request.form.get('password1')
-        password2 = request.form.get('password2')
+    # if request.method == 'POST':
+    #     email = request.form.get('email')
+    #     firstName = request.form.get('firstName')
+    #     password1 = request.form.get('password1')
+    #     password2 = request.form.get('password2')
 
-        if len(email) < 4:
-            flash (" Email must be at least 4 characters", category="error")
-        elif len(firstName) <2 :
-            flash (" name must be at least 2 characters", category="error")
-        elif password1 != password2:
-            flash (" Password dont much", category="error")
-        elif len(password2)<7:
-            flash (" Password too short", category="error")
-        else:
-            flash (" created", category="success")
+    #     if len(email) < 4:
+    #         flash (" Email must be at least 4 characters", category="error")
+    #     elif len(firstName) <2 :
+    #         flash (" name must be at least 2 characters", category="error")
+    #     elif password1 != password2:
+    #         flash (" Password dont much", category="error")
+    #     elif len(password2)<7:
+    #         flash (" Password too short", category="error")
+    #     else:
+    #         flash (" created", category="success")
 
-    return render_template("signup.html")
+    return render_template("sign_up.html")
