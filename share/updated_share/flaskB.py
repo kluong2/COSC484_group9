b7b93a1a42e1bae06595ebfb6ea9241b3c4b4b1a
henok <hassef1@students.towson.edu>
2021-04-11 21:57:09 -0400

    posts added

--- share/updated_share/flaskB.py
+++ share/updated_share/flaskB.py
@@ -1,14 +1,30 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for
 app = Flask(__name__)
+
+posts= [
+    {
+        'author' : 'Henok Assefa',
+        'title' : 'cosc 484',
+        'content': 'chapter one',
+        'date_posted': 'April 11, 2021'
+    },
+    {
+        'author' : 'Abel',
+        'title' : 'cosc 484',
+        'content': 'chapter two',
+        'date_posted': 'April 11, 2021'
+    }
+
+]
 
 @app.route('/')
 @app.route('/home')
 def home():
-    return render_template ('home.html')
+    return render_template ('home.html', posts = posts)
 
 @app.route('/about')
 def about():
-    return render_template ('about.html')
+    return render_template ('about.html', title= 'About')
 
 if __name__ == '__main__':
     app.run(debug=True)
